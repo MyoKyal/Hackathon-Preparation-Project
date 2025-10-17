3ee37320da22698b73771d42a296dda0852343ce
Myo Kyal <myokyal48@gmail.com>
2025-10-17 13:33:11 +0630

    First Commit

---
+++ my-project/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { NavbarMenu } from "../../fakeData/data";
+
+function Navbar(){
+
+    return (
+        <div>
+            Navbar
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ my-project/fakeData/data.js
@@ -0,0 +1,27 @@
+export const NavbarMenu = [
+    {
+        id: 1,
+        title: "Home",
+        link: "/"
+    },
+    {
+        id: 2,
+        title: "About",
+        link: "/"
+    },
+    {
+        id: 3,
+        title: "Blog",
+        link: "/"
+    },
+    {
+        id: 4,
+        title: "Contact",
+        link: "/"
+    },
+    {
+        id: 5,
+        title: "Function",
+        link: "/"
+    }
+]
